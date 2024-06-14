a7461db8ed3748722025da06232c877d60abc310
Timo Reichl <thetredev@gmail.com>
2024-06-15 00:34:54 +0200

    vm: add install-docker.sh
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

---
+++ vm/install-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+distro=$(. /etc/os-release && echo "$ID")
+distro_version=$(. /etc/os-release && echo "$VERSION_CODENAME")
+
+apt-get update
+apt-get install ca-certificates curl
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/${distro}/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+cat > /etc/apt/sources.list.d/docker.list <<EOF
+deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${distro} ${distro_version} stable
+EOF
+
+apt-get update
+apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
+
+systemctl enable docker --now
