bdbc6bbb507a20915bcabe1f5cdf0a9fb9c90d80
Timo Reichl <thetredev@gmail.com>
2024-10-27 02:12:30 +0100

    vm/init-vm-default.sh: make script code more sensible
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- vm/init-vm-default.sh
+++ vm/init-vm-default.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -rf /etc/machine-id
+rm -rf /var/lib/dbus/machine-id /etc/machine-id
+
+dbus-uuidgen --ensure
 ln -sf /var/lib/dbus/machine-id /etc/machine-id
 
-rm -rf /var/lib/dbus/machine-id
-dbus-uuidgen --ensure
-
 curl -fsSL https://raw.githubusercontent.com/thetredev/dotfiles/main/vm/init-vm-cleanup-ssh.sh | bash
-poweroff
+
+${@}
